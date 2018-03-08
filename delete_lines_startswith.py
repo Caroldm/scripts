e616fe83d59645f7824aa65ef314275cc15512a8
Caroline Dias Monteiro <carol.dmonteiro@gmail.com>
2018-03-08 09:47:47 -0300

    Add files via upload

---
+++ delete_lines_startswith.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd 
+import numpy as np
+import csv
+import re
+
+path = '/Users/Carol/Desktop/Work/hg_ag/hg_ag_sigs_full/'
+
+# create folder for saving signatures
+folders = os.listdir('./')
+savefolder = 'hg_ag_sigs_full_no_headers'
+if savefolder not in folders:
+    os.mkdir(savefolder)
+
+#path = '/Users/Carol/Desktop/Work/hg_ag/hg_ag_sigs_full/'
+
+
+for filename in os.listdir(path):
+	if filename.endswith('.txt'):
+		#print filename
+		fname = filename.strip('.txt') 
+		lines = []
+		file = (path + filename)
+		df = pd.read_csv(file, sep='\t', comment='!', index_col=0)
+		#print data
+		#lines = data.readlines()
+		savefile = savefolder + '/' +  fname + '.txt'
+		df.to_csv(savefile)
+		
+
+
+
+        
---
+++ import_os.r
@@ -0,0 +1,44 @@
+import os
+import re
+
+path = '/Users/Carol/Desktop/GS/'
+
+
+#print os.listdir(path)
+for filename in os.listdir(path):
+	with open(path + filename,'rU') as data:
+		data=data.readlines()
+		words = ['SRA:']
+		keep = re.compile(r"|".join(words))
+		#print(filename)
+    	for line in data:
+         # if line contains match from words
+        	if keep.search(line):
+        		l = line.replace('!Sample_relation =','')
+        		name = filename.strip('.soft')
+        		print(name + l)
+
+
+
+# #source("https://bioconductor.org/biocLite.R")
+# #biocLite("SRAdb")
+
+# setwd('/Users/Carol/Desktop/GSMmetadata/')
+
+
+# library(GEOquery) 
+# library(SRAdb) 
+
+
+
+
+# #gds <- getGEO("GSM1847166") 
+# #Meta(gds)
+
+# mylist=("GSM1686397", "GSM1686398", "GSM1686399", "GSM1686400")
+
+# for (i in mylist){
+#   gds <- getGEO(i)
+#   Meta(gds)
+# }
+# write(gds, file='/Users/Carol/Desktop/GSMmetadata/)
