e424f55e5f1f52f9ea4cc40c90f117e3088336bf
Caroline Dias Monteiro <16257952+Caroldm@users.noreply.github.com>
2018-03-11 14:57:53 -0300

    Create import_xlrd.py

---
+++ import_xlrd.py
@@ -0,0 +1,20 @@
+import xlrd
+
+book = open_workbook('data.xls')
+s1 = book.sheet_by_index(0)
+s2 = book.sheet_by_index(1)
+
+gene_list = worksheet.cel(i,3)
+pmid = worksheet.cel(i,4)
+tissue = worksheet.cel(i,5)
+org = worksheet.cel(i,6)
+value = worksheet.cell(i,1)   
+
+
+col = book.Range("A1:A1")
+for i in col:
+ if i+1 != cell
+  print s2.i(0,0).value + (" pmid" + "tissue" + "org")  
+ else i==i
+  print s2.i(0,1) + "gene_list"
+  
