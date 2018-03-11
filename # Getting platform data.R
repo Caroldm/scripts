c8b5deb9f34dfadde94e537f3450d8c165f13ccb
Caroline Dias Monteiro <16257952+Caroldm@users.noreply.github.com>
2018-03-11 14:36:58 -0300

    Update # Getting platform data.R

--- # Getting platform data.R
+++ # Getting platform data.R
@@ -7,9 +7,9 @@ setwd('/Users/Carol/Desktop/')
 
 library(GEOquery) 
 
-# Read in the data
+# Read in data
 #x <- scan("/Users/Carol/Desktop/LadoE/gsm_11.txt", what="", sep="\n")
-#gsm_list <- strsplit(x, "[[:space:]]+")  # Separate elements by one or more whitepace
+#gsm_list <- strsplit(x, "[[:space:]]+")  # Separate elements by whitepace
 #for (i in gsm_list){
 #  gsm <- getGEO(i, destdir='/Users/Carol/Desktop/gsm_falta/')
 #}
@@ -19,15 +19,12 @@ gpl10558 <- getGEO('gpl10558')
 Meta(gpl10558)$title
 
 head(Meta(gpl10558)$series_id)
-
 length(Meta(gpl10558)$series_id)
-
 head(Meta(gpl10558)$sample_id)
-
 length(Meta(gpl10558)$sample_id)
 
-# Download all the samples
 
+# Download all samples
 gsmids <- Meta(gpl10558)$sample_id
 gsmlist <- sapply(gsmids,getGEO)
 data <- names(gsmlist)
