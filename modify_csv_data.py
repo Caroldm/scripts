d25d66522fcf724ffcc733829300a0302a32f377
Caroline Dias Monteiro <16257952+Caroldm@users.noreply.github.com>
2018-03-11 15:20:23 -0300

    Update modify_csv_data.py

--- modify_csv_data.py
+++ modify_csv_data.py
@@ -4,8 +4,6 @@
 ################################
 ################################
 
-
-
 import csv
 out = open ('/Users/carol/Desktop/chea_test.csv','rt', encoding='utf8') ## keept getting error iterator, should return strings, not bytes
 data =  csv.reader(out)
@@ -23,7 +21,8 @@ output = csv.writer(out)
 
 
 for row in new_data:
-   output.writerow(row)    
+   output.writerow(row)  
+   
 out.close() 
 
 
@@ -37,15 +36,12 @@ out.close()
 ################################
 ################################
 
-
 import csv
 out = open ('/Users/carol/Desktop/cheabackground1.csv','rt', encoding='UTF-8')  
 data =  csv.reader(out)
 
-
 new_data = [[row[1]+ '-' + row[4]+'-'+row[5]+'-'+row[6] +'-'+ row[7], row[3]] for row in data] 
 #new_data = [[row[2]+'-'+row[5]+'-'+row[6] +'-'+ row[7], row[3]] for row in data] 
-
 
 #Dict
 my_dict = {}
@@ -58,14 +54,15 @@ for row in new_data:
 new_data = [[my_key,my_dict[my_key]] for my_key in my_dict]
 #print (new_data)
 
-
 #Output
 out = open('/Users/carol/new_dataf.csv','wt') 
 output = csv.writer(out)  
 
 for row in new_data:
-   output.writerow(row)    
+   output.writerow(row)  
+   
 out.close()
+
 
 
 
@@ -77,7 +74,6 @@ out.close()
 ################################
 ################################
 
-
 import csv
 out = open ('/Users/carol/Desktop/cheabackground1.csv','rt', decode='UTF-8')  # open the input file rename it out
 data =  csv.reader(out) # read inputed file
@@ -85,10 +81,8 @@ data =  csv.reader(out) # read inputed file
 new_keys = [row[1]+ '-' + row[4]+'-'+row[5]+'-'+row[6] +'-'+ row[7] for row in data] 
 new_values = [row[3] for row in data]
 
-
-aggregate_values = {} # An empty dictionary
-
-# Iterate across the paired lists together
+aggregate_values = {} # Empty dict
+# Iterate across paired lists 
 
 for key, value in zip(new_keys, new_values): 
     if key not in aggregate_values:
@@ -102,12 +96,12 @@ output = csv.writer(out)
 # printed output
 for key in aggregate_values:
     print (key + ' ' + ' '.join(aggregate_values[key])
-    
-
+ 
 out.close()
 
            
            
+
            
 ################################
 ################################
@@ -117,7 +111,6 @@ out.close()
 ################################
 
 import csv
-
 out = open ('/Users/carol/Desktop/cheabackground1.csv','rt', encoding = 'UTF-8', errors = 'ignore')  
 data =  csv.reader(out)
 
@@ -131,12 +124,10 @@ for row in new_data:
       my_dict[row[0]] = row[1]
 
 new_data = [[my_key,my_dict[my_key]] for my_key in my_dict]
-
-print (new_data)
+#print (new_data)
 
 out = open('/Users/carol/new_dataf1.csv','wt') 
 output = csv.writer(out)  
-
 
 out.close()
 
